7ddf164e39309955d94837552d5770e435dfeb46
dayani713 <dbandara163@gmail.com>
2025-03-25 19:39:33 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,28 +1,13 @@
-const { cmd, commands } = require('../command');
+const express = require('express');
 
-cmd({
-    pattern: "alive",
-    desc: "Check if the bot is online.",
-    category: "main",
-    filename: __filename
-},
-async (robin, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-        const config = await readEnv();
-        
-        // Check if the necessary config values are loaded properly
-        if (!config.ALIVE_IMG || !config.ALIVE_MSG) {
-            console.log('Config values are missing!');
-            return reply('Error: Missing configuration values!');
-        }
+const server = express();
 
-        console.log('Sending alive message...');
-        await robin.sendMessage(from, {
-            image: { url: config.ALIVE_IMG },
-            caption: config.ALIVE_MSG
-        }, { quoted: mek });
-    } catch (e) {
-        console.log('Error:', e);
-        reply(`An error occurred: ${e.message || e}`);
-    }
-});
+server.all('/', (req, res)=>{
+    res.send('Your bot is alive!')
+})
+
+function keepAlive(){
+    server.listen(3000, ()=>{console.log("Server is Ready!")});
+}
+
+module.exports = keepAlive;
